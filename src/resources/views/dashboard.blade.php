b773359715c00030983301c361be875533c7acba
Cory Hrycko <chrycko9491@gmail.com>
2021-10-11 23:59:15 -0400

    finnished api gui stuff

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -10,11 +10,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         @foreach($movies as $movie)
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div id="{{$movie->id}}" class="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div x-cloak class="p-6 bg-white border-b border-gray-200">
-                {{$movie->title}}  {{$movie->format}}  {{$movie->length}}  {{$movie->release_year}}   {{$movie->rating}} <button onclick="deleteMovie({{$movie->id}})">delete</button>
-                <button onclick="getListOfMovies()">Movies!</button>    
-                <button onclick="editMovie({{$movie->id}})">Edit!</button>    
+                {{$movie->title}}  {{$movie->format}}  {{$movie->length}}  {{$movie->release_year}}   {{$movie->rating}} 
+                <button class="focus:outline-none focus:ring-4 focus:ring-red-500 focus:ring-opacity-50"  onclick="deleteMovie({{$movie->id}})">delete</button>
+                <button class="focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50" onclick="editMovie({{$movie->id}})">Edit!</button>    
             </div>
             </div>
         @endforeach
@@ -23,6 +23,7 @@
 
     <script>
 
+    // Healpers
     htmlTemplate = () => {
         return '<form id="myForm">' +
             '<div id="swal1-content" style="display: block;">Please enter the title</div>' +
@@ -37,6 +38,38 @@
 			'<div><input type="text" id="swal-input-rating" class="swal2-input"></div><br>'+
             '</form>';
     }
+    displayResults = (json,result) => {
+        if (!json.hasOwnProperty('message')) {
+            Swal.fire(
+                `Movie ${result}!`,
+                '',
+                'success'
+            );
+                location.reload();
+        } else {
+            Swal.fire(
+                'Warning!',
+                json.message +
+                'error'
+            );
+        }
+    };
+    elementTargets = () => {
+        return JSON.stringify({
+                        title: document.getElementById('swal-input-title').value,
+                        format: document.getElementById('swal-input-format').value,
+                        length: document.getElementById('swal-input-length').value,
+                        release_year: document.getElementById('swal-input-release').value,
+                        rating: document.getElementById('swal-input-rating').value,
+                    });
+    } 
+    getHeaders = () => {
+        return {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': '{{ csrf_token() }}',
+        }
+    }
     getListOfMovies = async () => {
         const location = window.location.hostname;
         const settings = {
@@ -48,16 +81,17 @@
         };
         try {
             const fetchResponse = await fetch(`http://${location}/api/movie/`, settings);
-            const data = await fetchResponse.json();
-            return data;
+            return await fetchResponse.json();
         } catch (e) {
             return e;
         }    
     }
 
+
+    // Modals
     editMovie = async (id) => {
 		const options = {
-			title: 'Edit a Client',
+			title: 'Edit a Movie',
 			icon: 'question',
 			showCancelButton: true,
 			confirmButtonColor: '#3085d6',
@@ -69,52 +103,26 @@
                 const location = window.location.hostname;
                 const settings = {
                     method: 'put',
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                    },
-                    body: JSON.stringify({
-                        title: document.getElementById('swal-input-title').value,
-                        format: document.getElementById('swal-input-format').value,
-                        length: document.getElementById('swal-input-length').value,
-                        release_year: document.getElementById('swal-input-release').value,
-                        rating: document.getElementById('swal-input-rating').value,
-                    })
+                    headers: getHeaders(),
+                    body: elementTargets()
                 };
                 try {
-                    const fetchResponse = await fetch(`http://${location}/api/movie/`.id, settings);
-                    const data = await fetchResponse.json();
-                    return data;
+                    const fetchResponse = await fetch(`http://${location}/api/movie/${id}`, settings);
+                    return await fetchResponse.json();
                 } catch (e) {
                     return e;
                 }    
 
             }
         })
-                    .then((json) => {
-					if (!json.hasOwnProperty('message')) {
-						Swal.fire(
-							'Movie Created!',
-							'',
-							'success'
-						);
-                        location.reload();
-					} else {
-                        console.log(json.hasOwnProperty('message'))
-						Swal.fire(
-							'Warning!',
-							json.message +
-							'error'
-						);
-					}
-				});
+        .then((json) => {
+                displayResults(json, 'Edited');
+            });
     }
-    
 
     newMovie = async () => {
 		const options = {
-			title: 'Create a new Client',
+			title: 'Create a new Movie',
 			icon: 'question',
 			showCancelButton: true,
 			confirmButtonColor: '#3085d6',
@@ -126,64 +134,50 @@
                 const location = window.location.hostname;
                 const settings = {
                     method: 'POST',
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                    },
-                    body: JSON.stringify({
-                        title: document.getElementById('swal-input-title').value,
-                        format: document.getElementById('swal-input-format').value,
-                        length: document.getElementById('swal-input-length').value,
-                        release_year: document.getElementById('swal-input-release').value,
-                        rating: document.getElementById('swal-input-rating').value,
-                    })
+                    headers: getHeaders(),
+                    body: elementTargets()
                 };
                 try {
                     const fetchResponse = await fetch(`http://${location}/api/movie/`, settings);
-                    const data = await fetchResponse.json();
-                    return data;
+                    return await fetchResponse.json();
                 } catch (e) {
                     return e;
                 }    
 
             }
         })
-                    .then((json) => {
-					if (!json.hasOwnProperty('message')) {
-						Swal.fire(
-							'Movie Created!',
-							'',
-							'success'
-						);
-                        location.reload();
-					} else {
-                        console.log(json.hasOwnProperty('message'))
-						Swal.fire(
-							'Warning!',
-							json.message +
-							'error'
-						);
-					}
-				});
+            .then((json) => {
+                displayResults(json, 'Created');
+            });
     }
 
     deleteMovie = async (id) => {
+		const options = {
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+		};
+        return Swal.fire(options).then(async (result) => {
         const location = window.location.hostname;
         const settings = {
             method: 'DELETE',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            }
+            headers: getHeaders(),
         };
-        try {
-            const fetchResponse = await fetch(`http://${location}/api/movie/`.id, settings);
-            const data = await fetchResponse.json();
-            return data;
-        } catch (e) {
-            return e;
-        }    
+        if (result.value) {
+            try {
+                const fetchResponse = await fetch(`http://${location}/api/movie/${id}`, settings);
+                return await fetchResponse.json();
+            } catch (e) {
+                return e;
+            }
+        }})   
+        .then(async (json) => {
+                displayResults(json, 'Deleted');
+            }); 
     }
     </script>
 </x-app-layout>
